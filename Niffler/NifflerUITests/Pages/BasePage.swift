16774bcaa45fb3bddc2d2b3dd7a3e52307b69405
garaga07 <garaga07@yandex.ru>
2024-12-11 20:14:37 +0200

    Выполнение дз №25

--- Niffler/NifflerUITests/Pages/BasePage.swift
+++ Niffler/NifflerUITests/Pages/BasePage.swift
@@ -6,4 +6,17 @@ class BasePage {
     }
     
     let app: XCUIApplication
+    
+    func launchAppWithoutLogin(){
+        XCTContext.runActivity(named: "Запуск приложения") { _ in
+            app.launchArguments = ["RemoveAuthOnStart"]
+            app.launch()
+        }
+    }
+    
+    func waitForElement(_ element: XCUIElement, timeout: TimeInterval = 1, message: String, file: StaticString = #file, line: UInt = #line) {
+        XCTContext.runActivity(named: "Ожидание элемента") { _ in
+            XCTAssertTrue(element.waitForExistence(timeout: timeout), message, file: file, line: line)
+        }
+    }
 }
